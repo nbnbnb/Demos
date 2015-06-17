c449ff285f948159c3ec97ae8e21bd823f56b7f8
jinzhanga <jinzhanga@Ctrip.com>
2015-06-17 19:19:50 +0800

    Add Part 02

---
+++ DotNet/CtripSOA2/WebHelloWorldService/HelloServiceImpl.cs
@@ -0,0 +1,29 @@
+﻿using CServiceStack.Common.Types;
+using CServiceStack.ServiceHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebHelloWorldService
+{
+    public class HelloServiceImpl : IHelloWorldService
+    {
+        //[Route("/sayHello")]
+        public HelloResponseType Hello(HelloRequestType request)
+        {
+            return new HelloResponseType
+            {
+                IsSuccess = true,
+                Result = request.OrderId.ToString() + " Is Process Success"
+            };
+        }
+
+        //[Route("/check")]
+        public CheckHealthResponseType CheckHealth(CheckHealthRequestType request)
+        {
+            return new CheckHealthResponseType();
+        }
+    }
+}

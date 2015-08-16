70388486a22dd96f7290ba00a65445773b5f74ab
ZhangJin <nbnbnb.zhang@live.com>
2015-08-16 17:10:40 +0800

    Sync Home

--- DotNet/CtripSOA2/ClientTest/Program.cs
+++ DotNet/CtripSOA2/ClientTest/Program.cs
@@ -19,13 +19,12 @@ namespace ClientTest
     {
         static void Main(string[] args)
         {
-            //SimpleRequest();
-            //SimpleRequest();
             Test_HelloService_WebClient();
-            Test_HelloService_HttpWebRequest();
-            Test_HelloService_HttpClient();
+            //Test_HelloService_HttpWebRequest();
             Console.ReadKey();
         }
+
+        #region Test
         private static void Test_SettlementOpenAPI()
         {
             var webHost = "http://ws.settlement.ttd.fat6.qa.nt.ctripcorp.com/ticket-settlement-openapi/api";
@@ -101,17 +100,92 @@ namespace ClientTest
             string data = @"<?xml version=""1.0"" encoding=""utf-8""?><HelloRequest xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns=""http://soa.ctrip.com/thingstodo/order/settlementopenapi/v1""><OrderId>123456</OrderId><UnitQuantity>4</UnitQuantity><EndDate>2015-08-04T17:56:43.5959493+08:00</EndDate><Price>99.99</Price></HelloRequest>";
             byte[] buf = System.Text.Encoding.UTF8.GetBytes(data);
             client.Headers.Set(HttpRequestHeader.ContentType, "application/xml; charset=utf-8");
-            string url = "http://localhost:9636/Hello.xml";  // IIS
-            byte[] resBuf = client.UploadData(url, "POST", buf);
-            Console.WriteLine(Encoding.UTF8.GetString(resBuf));
+            string url = "http://www.zhangjin.me:9636/Hello.xml";  // IIS
+            //string url = "http://localhost:59127/Hello.xml";  // IIS Express
+            byte[] resBuf = client.UploadData(url, buf);
+            string res_1 = Encoding.UTF8.GetString(resBuf, 0, resBuf.Length);
+            string res_2 = String.Empty;
+            Console.WriteLine(res_1);  // 头 3 个字节为 BOM 头，输出时将会显示为"?"
+            Console.WriteLine("-----");
+            using (MemoryStream ms = new MemoryStream(resBuf))
+            {
+                using (StreamReader sr = new StreamReader(ms))  // 让 StreamReader 来自动处理 BOM 头
+                {
+                    res_2 = sr.ReadToEnd();
+                    Console.WriteLine(res_2);
+                }
+            }
+            Console.WriteLine(res_1 == res_2);
+            Console.WriteLine(res_1.Length);
+            Console.WriteLine(res_2.Length);
         }
 
         private static void Test_HelloService_HttpWebRequest()
         {
-            // 这个发送 HTTP 请求最底
+            // 发送 HTTP 底层请求
             Console.WriteLine("-----");
-            string url = "http://localhost:9636/Hello.xml";  // IIS
+            string url = "http://www.zhangjin.me:9636/Hello.xml";  // IIS
+            //string url = "http://localhost:59127/Hello.xml";  // IIS Express
             WebRequest request = HttpWebRequest.Create(url);
+            string data = @"<?xml version=""1.0"" encoding=""utf-8""?><HelloRequest xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns=""http://soa.ctrip.com/thingstodo/order/settlementopenapi/v1""><OrderId>123456</OrderId><UnitQuantity>4</UnitQuantity><EndDate>2015-08-04T17:56:43.5959493+08:00</EndDate><Price>99.99</Price></HelloRequest>";
+            byte[] buf = Encoding.UTF8.GetBytes(data);
+            request.ContentLength = buf.Length;  // 这些参数都必须设置
+            request.Method = "POST"; // 这些参数都必须设置
+            request.ContentType = "application/xml; charset=utf-8";
+            using (Stream stream = request.GetRequestStream())
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(data);
+                }
+            }
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    int size = 100;
+                    byte[] btking = new byte[size];
+                    List<byte> res = new List<byte>();
+                    int endsize = 0;
+                    while ((endsize = stream.Read(btking, 0, size)) == size)
+                    {
+                        res.AddRange(btking);
+                    }
+                    byte[] endbuf = new byte[endsize];
+                    Array.Copy(btking, endbuf, endsize);
+                    res.AddRange(endbuf);
+                    Console.WriteLine(Encoding.UTF8.GetString(res.ToArray()));
+
+                    /*
+                    using (StreamReader sr = new StreamReader(stream))
+                    {
+                        Console.WriteLine(sr.ReadToEnd());
+                    }
+                    */
+                }
+            }
+        }
+
+        private static void Test_HelloService_HttpWebRequest_Proxy()
+        {
+            /*
+            IsBypassed 方法用于确定在访问 Internet 资源时是否不使用代理服务器。
+            BypassProxyOnLocal 和 BypassList 属性控制 IsBypassed 方法的返回值。
+            在以下任何条件下，IsBypassed 均返回 true：
+            如果 BypassProxyOnLocal 为 true 且 host 是本地 URI。通过在 URI 中省略句点(.) 来标识本地请求，如“http://webserver/”。
+                        如果 host 与 BypassList 中的正则表达式匹配。
+            如果 Address 为 空引用（在 Visual Basic 中为 Nothing）。
+            所有其他条件返回 false。
+            */
+            Console.WriteLine("-----");
+            string url = "http://www.zhangjin.me:9636/Hello.xml";  // IIS
+            //string url = "http://localhost:59127/Hello.xml";  // IIS Express
+            Uri uri = new Uri(url);
+            //WebProxy proxy = new WebProxy("http://localhost:8888", false); // Fiddler 会自动添加一个 8888 端口的本地代理
+            //WebRequest.DefaultWebProxy = proxy; // 后续创建的 WebRequest 对象都会使用这个代理
+            //Console.WriteLine(proxy.IsBypassed(uri)); // 用来判断请求是否使用代理，只有返回 False 的请求才会使用代理；所以 URL 一定要设置为非本地的才会被 Fiddler 捕获到
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
             string data = @"<?xml version=""1.0"" encoding=""utf-8""?><HelloRequest xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns=""http://soa.ctrip.com/thingstodo/order/settlementopenapi/v1""><OrderId>123456</OrderId><UnitQuantity>4</UnitQuantity><EndDate>2015-08-04T17:56:43.5959493+08:00</EndDate><Price>99.99</Price></HelloRequest>";
             byte[] buf = Encoding.UTF8.GetBytes(data);
             request.ContentLength = buf.Length;  // 这些参数都必须设置
@@ -140,8 +214,8 @@ namespace ClientTest
         {
             Console.WriteLine("-----");
             HttpClient client = new HttpClient();
-            string url = "http://localhost:9636/Hello.xml";  // IIS
-
+            string url = "http://www.zhangjin.me:9636/Hello.xml";  // IIS
+            //string url = "http://localhost:59127/Hello.xml";  // IIS Express
             string data = @"<?xml version=""1.0"" encoding=""utf-8""?><HelloRequest xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns=""http://soa.ctrip.com/thingstodo/order/settlementopenapi/v1""><OrderId>123456</OrderId><UnitQuantity>4</UnitQuantity><EndDate>2015-08-04T17:56:43.5959493+08:00</EndDate><Price>99.99</Price></HelloRequest>";
             byte[] buf = Encoding.UTF8.GetBytes(data);
             StringContent conent = new StringContent(data);
@@ -150,7 +224,7 @@ namespace ClientTest
             HttpResponseMessage result = message.Result;
             Task<Stream> btking = result.Content.ReadAsStreamAsync();
             btking.Wait();
-            using (Stream stream=btking.Result)
+            using (Stream stream = btking.Result)
             {
                 using (StreamReader sr = new StreamReader(stream))
                 {
@@ -158,5 +232,6 @@ namespace ClientTest
                 }
             }
         }
+        #endregion
     }
 }

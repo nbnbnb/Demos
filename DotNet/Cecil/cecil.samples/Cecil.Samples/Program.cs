619a6fdcf2748c9873d86a0d0455705337144416
ZhangJin <nbnbnb.zhang@live.com>
2015-08-24 23:00:46 +0800

    Add Mono Cecil

---
+++ DotNet/Cecil/cecil.samples/Cecil.Samples/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Cecil.Samples.Framework;
+using Mono.Cecil;
+using NUnit.Framework;
+using TypeAttributes = Mono.Cecil.TypeAttributes;
+
+#region Cheat
+using SamplesAttribute = NUnit.Framework.TestFixtureAttribute;
+using SampleAttribute = NUnit.Framework.TestAttribute;
+#endregion
+
+namespace Cecil.Samples
+{
+	[Samples]
+	public class SamplesDirectory
+	{
+		[Sample]
+		public void AddAttributeToMethod()
+		{
+			RunSample("AddAttributeToMethod");
+		}
+
+		[Sample]
+		public void ReplaceMethodCall()
+		{
+			RunSample("ReplaceMethodCall");
+		}
+
+		private void RunSample(string sampleName)
+		{
+			var dataPath = Path.GetDirectoryName(GetType().Assembly.Location);
+		
+			Assert.IsNotNull(dataPath);
+
+			var sampleAssemblyPath = Path.Combine(dataPath, sampleName + ".Sample.dll");
+
+			if (!File.Exists(sampleAssemblyPath))
+				Assert.Fail("Could not find sample named " + sampleName);
+
+			var targetAssemblyPath = Path.Combine(dataPath, sampleName + ".Target.dll");
+
+			if (!File.Exists(targetAssemblyPath))
+				Assert.Fail("Could not find target named " + sampleName);
+
+			var targetModule = ModuleDefinition.ReadModule(targetAssemblyPath);
+			if (!IsMarked(targetModule))
+			{
+				var sampleAssembly = Assembly.LoadFrom(sampleAssemblyPath);
+				var sampleType = sampleAssembly.GetTypes().FirstOrDefault(t => typeof(ISample).IsAssignableFrom(t));
+
+				Assert.IsNotNull(sampleType, "Could not find a sample type to run");
+
+				var sample = (ISample)Activator.CreateInstance(sampleType, targetAssemblyPath);
+
+				sample.TargetModule.Types.Add(
+					new TypeDefinition("", "IMarked", TypeAttributes.Interface | TypeAttributes.Abstract));
+
+				sample.Run();				
+			}
+
+			var targetAssembly = Assembly.LoadFrom(targetAssemblyPath);
+			var targetType = targetAssembly.GetTypes().FirstOrDefault(t => typeof(ITarget).IsAssignableFrom(t));
+
+			Assert.IsNotNull(targetType, "Could not find a target type to run");
+
+			var target = (ITarget) Activator.CreateInstance(targetType);
+
+			target.Run();
+		}
+
+		private static bool IsMarked(ModuleDefinition targetModule)
+		{
+			return targetModule.Types.Any(t => t.Name == "IMarked");
+		}
+	}
+}
---
+++ DotNet/Cecil/cecil.samples/Cecil.Samples/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cecil.Samples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cecil.Samples")]
+[assembly: AssemblyCopyright("Copyright © Jb Evain 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8dc387f8-81fd-4080-b549-999a6783f6f1")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

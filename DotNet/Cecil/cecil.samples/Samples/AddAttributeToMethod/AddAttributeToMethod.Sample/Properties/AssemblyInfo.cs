0bb57b2d3b834ada49593724e42b826d79afdb37
ZhangJin <nbnbnb.zhang@live.com>
2015-08-24 23:00:46 +0800

    Add Mono Cecil

---
+++ DotNet/Cecil/cecil.samples/Samples/AddAttributeToMethod/AddAttributeToMethod.Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AddAttributeToMethod.Sample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cecil.Samples")]
+[assembly: AssemblyCopyright("Copyright © Jb Evain 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7efa1b6-a371-45bd-8d21-7f1d71621236")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotNet/Cecil/cecil.samples/Samples/AddAttributeToMethod/AddAttributeToMethod.Target/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AddAttributeToMethod.Target")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cecil.Samples")]
+[assembly: AssemblyCopyright("Copyright © Jb Evain 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7070d9d-d9f8-48e4-b84d-7cb8db651829")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

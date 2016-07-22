a46303bb86bcfbd3635dd63cfc4a312ddc051885
ZhangJin <nbnbnb.zhang@live.com>
2016-07-22 23:45:22 +0800

    Update DbContext Demo

---
+++ DotNet/BookDemo/DbContext/Back/Chap2/Model/IObjectWithState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public interface IObjectWithState
+    {
+        State State { get; set; }
+    }
+
+}

ce5db240cfad93a01cfc192eb3df5ece03dfd2a8
ZhangJin <nbnbnb.zhang@live.com>
2016-07-22 23:31:15 +0800

    Sync

---
+++ DotNet/BookDemo/ASPNETPattern/ASPPatterns.Chap7.UnitOfWork/ASPPatterns.Chap7.UnitOfWork.Model/AggregateService.cs
@@ -0,0 +1,26 @@
+﻿using ASPPatterns.Chap7.UnitOfWork.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASPPatterns.Chap7.UnitOfWork.Model
+{
+    public class AggregateService<TAggregate> where TAggregate : IAggregateRoot
+    {
+        private IAggregateRepository<TAggregate> _repository;
+        public AggregateService(IAggregateRepository<TAggregate> repository)
+        {
+            _repository = repository;
+        }
+
+        public void DoSomething(TAggregate toAdd, TAggregate toUpdate, TAggregate toDelete)
+        {
+            _repository.Save(toUpdate);
+            _repository.Add(toAdd);
+            _repository.Remove(toDelete);
+
+            _repository.UnitOfWork.Commit();
+        }
+    }
+}
---
+++ DotNet/BookDemo/ASPNETPattern/ASPPatterns.Chap7.UnitOfWork/ASPPatterns.Chap7.UnitOfWork.Repository/AggregateRepository.cs
@@ -0,0 +1,55 @@
+﻿using ASPPatterns.Chap7.UnitOfWork.Infrastructure;
+using ASPPatterns.Chap7.UnitOfWork.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASPPatterns.Chap7.UnitOfWork.Repository
+{
+    public class AggregateRepository<TAggregate> : IAggregateRepository<TAggregate>, IAggregateUnitOfWorkRepository<TAggregate> where TAggregate : IAggregateRoot
+    {
+        private IAggregateUnitOfWork<TAggregate> _unitOfWork;
+
+        public IAggregateUnitOfWork<TAggregate> UnitOfWork
+        {
+            get
+            {
+                return _unitOfWork;
+            }
+        }
+
+        public AggregateRepository(IAggregateUnitOfWork<TAggregate> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void PersistUpdateOf(TAggregate entity)
+        {
+            // ADO.NET code to update the entity...
+        }
+        public void PersistCreationOf(TAggregate entity)
+        {
+            // ADO.NET code to add the entity...
+        }
+        public void PersistDeletionOf(TAggregate entity)
+        {
+            // ADO.NET code to delete the entity...
+        }
+
+        public void Save(TAggregate item)
+        {
+            _unitOfWork.RegisterAmended(item, this);
+        }
+
+        public void Add(TAggregate item)
+        {
+            _unitOfWork.RegisterNew(item, this);
+        }
+
+        public void Remove(TAggregate item)
+        {
+            _unitOfWork.RegisterRemoved(item, this);
+        }
+    }
+}

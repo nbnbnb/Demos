8da3f17ff0eb190c9117b29b4feca11b9caf4658
ZhangJin <nbnbnb.zhang@gmail.com>
2016-10-04 22:33:40 +0800

    new

---
+++ DotNet/WCF/用户名密码客户端验证/Service/CustomValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.Linq;
+using System.Text;
+
+namespace Service
+{
+    public class CustomValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            // 如果验证不通过，则直接抛出异常接口
+            // 验证通过，则不做任何处理
+        }
+    }
+}
